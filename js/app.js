da7f7c3bf73198c4ee7b0787af64e60c7c2fa2ed
Chelsea Niemeier <32147958+chelseaniemeier@users.noreply.github.com>
2017-10-25 00:09:32 -0400

    added hamburger toggle menu
    added toggle menu in the nav

--- js/app.js
+++ js/app.js
@@ -1,16 +1,40 @@
-//Hide hint
-$("form span").hide();
 
-//Test to see if they left input field blank, and if so, show hint
-function validateForm() {
-    var x = document.forms["newsletterEmail"]["email"].value;
-    if (x == "") {
-        $("form span").show();
-        return false;
-    } 
-}
 
-//When hint shows, and they retry to add email, hide hide again
-$("#email").focus(function(){
-    $("form span").hide();
-})
+$(document).ready(function(){
+    
+        //HAMBURGER MENU -- TOGGLE//
+
+        $('.menu_exit').click(function(){
+            $('.sidebar_menu').removeClass("show_menu");
+            $('.toggle_menu').addClass("opacity_one").removeClass("toggle_menu_zindex-change");
+        });
+    
+        $('.toggle_menu').click(function(){
+            $('.sidebar_menu').addClass("show_menu");
+            $('.toggle_menu').removeClass("opacity_one").addClass("toggle_menu_zindex-change");
+        });
+    
+    });
+    
+
+
+
+        //E-MAIL JOIN NEWSLETTER IN FOOTER//
+
+        //Hide hint
+        $("form span").hide();
+
+        //Test to see if they left input field blank, and if so, show hint
+        function validateForm() {
+            var x = document.forms["newsletterEmail"]["email"].value;
+            if (x == "") {
+                $("form span").show();
+                return false;
+            } 
+        }
+
+        //When hint shows, and they retry to add email, hide hide again
+        $("#email").focus(function(){
+            $("form span").hide();
+        })
+
