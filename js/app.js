ad0ad1759d6715f24f7e07f07aa239f5e7d7b336
Chelsea Niemeier <cniemeier@derbymuseum.org>
2017-11-01 16:16:43 -0400

    Added contact page

--- js/app.js
+++ js/app.js
@@ -17,22 +17,34 @@ $(document).ready(function(){
     });
     
 
-        //E-MAIL JOIN NEWSLETTER IN FOOTER//
+//E-MAIL JOIN NEWSLETTER IN FOOTER//
 
-        //Hide hint
-        $("form span").hide();
+//Hide hint
+$("form span").hide();
 
-        //Test to see if they left input field blank, and if so, show hint
-        function validateForm() {
-            var x = document.forms["newsletterEmail"]["email"].value;
-            if (x == "") {
-                $("form span").show();
-                return false;
-            } 
-        }
+//Test to see if they left input field blank, and if so, show hint
+function validateForm() {
+    var x = document.forms["newsletterEmail"]["email"].value;
+    if (x == "") {
+        $("form span").show();
+        return false;
+    } 
+}
 
-        //When hint shows, and they retry to add email, hide hide again
-        $("#email").focus(function(){
-            $("form span").hide();
-        })
+//When hint shows, and they retry to add email, hide hide again
+$("#email").focus(function(){
+    $("form span").hide();
+})
 
+
+
+//Google Map//
+
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(38.205377, -85.771569),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+var map = new google.maps.Map(document.getElementById("contactMap"), mapOptions);
+}
